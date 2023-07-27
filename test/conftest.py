86f6359eae5d5ff9c90f863cca2bcd95120dabb6
olga <olgamursina@yandex.ru>
2023-07-27 15:35:49 +0300

    15/03/23

--- test/conftest.py
+++ test/conftest.py
@@ -1,16 +1,18 @@
-import pytest
-import string
 import random
+import string
+
+import pytest
+
 
 @pytest.fixture
 def create_user():
-    user={}
-    email=''.join(random.choices(string.ascii_lowercase,k=4))+'@hophop.com'
-    name=''.join(random.choices(string.ascii_lowercase,k=10))+' '+'Pupkin'
-    gender=random.choice(['male','female'])
-    status=random.choice(['active','inactive'])
-    user['email']=email
-    user['name']=name
-    user['gender']=gender
-    user['status']=status
+    user = {}
+    email = ''.join(random.choices(string.ascii_lowercase, k=4)) + '@hophop.com'
+    name = ''.join(random.choices(string.ascii_lowercase, k=10)) + ' ' + 'Pupkin'
+    gender = random.choice(['male', 'female'])
+    status = random.choice(['active', 'inactive'])
+    user['email'] = email
+    user['name'] = name
+    user['gender'] = gender
+    user['status'] = status
     return user
--- test/test_4.py
+++ test/test_4.py
@@ -3,52 +3,46 @@ import requests
 
 def test_function4():
     bodies = [{"email": "hopoipa@opop.com", "name": "opop", "gender": "male", "status": "active"},
-        {"email": "hopozopa@opop.com", "name": "zopop", "gender": "male", "status": "active"},
-        {"email": "hoertozopa@opop.com", "name": "zofeFpop", "gender": "male", "status": "active"},
-        {"email": "hsafrtozopa@opop.com", "name": "zosacfpop", "gender": "female", "status": "active"}, ]
+              {"email": "hopozopa@opop.com", "name": "zopop", "gender": "male", "status": "active"},
+              {"email": "hoertozopa@opop.com", "name": "zofeFpop", "gender": "male", "status": "active"},
+              {"email": "hsafrtozopa@opop.com", "name": "zosacfpop", "gender": "female", "status": "active"}, ]
     headers = {"Authorization": "Bearer c3842a0fdeb2501dc1288ddbc7df815f22367a29057ca2672e680ff81e61d3f3"}
 
     for element in bodies:
-        b = requests.post('https://gorest.co.in/public/v2/users', headers=headers, data=element)
+        b = requests.post("https://gorest.co.in/public/v2/users", headers=headers, data=element)
         print(b.json())
 
 
 def test_function5():
-    data = {
-        "email": "hopsfsdgoip@ofspop.com",
-        "name": "opopd",
-        "gender": "male",
-        "status": "afsba"
-    }
+    data = {"email": "hopsfsdgoip@ofspop.com", "name": "opopd", "gender": "male", "status": "afsba"}
     headers = {"Authorization": "Bearer c3842a0fdeb2501dc1288ddbc7df815f22367a29057ca2672e680ff81e61d3f3"}
-    b = requests.post('https://gorest.co.in/public/v2/users', headers=headers, data=data)
-    assert data==b.json()
+    b = requests.post("https://gorest.co.in/public/v2/users", headers=headers, data=data)
+    assert data == b.json()
     print(b.status_code)
     print(b.json())
 
+
 def test_function7():
-    data = {
-        "email": "hopsfssadwrdgoip@ofspop.com",
-        "name": "opofrpd",
-        "gender": "female",
-        "status": ["active", "inactive"]
-    }
+    data = {"email": "hopsfssadwrdgoip@ofspop.com", "name": "opofrpd", "gender": "female",
+        "status": ["active", "inactive"]}
     headers = {"Authorization": "Bearer c3842a0fdeb2501dc1288ddbc7df815f22367a29057ca2672e680ff81e61d3f3"}
-    b = requests.post('https://gorest.co.in/public/v2/users', headers=headers, data=data)
-    assert b.status_code==200
+    b = requests.post("https://gorest.co.in/public/v2/users", headers=headers, data=data)
+    assert b.status_code == 200
     print(b.status_code)
     print(b.json())
+
 
 def test_function8(create_user):
     headers = {"Authorization": "Bearer c3842a0fdeb2501dc1288ddbc7df815f22367a29057ca2672e680ff81e61d3f3"}
-    b = requests.post('https://gorest.co.in/public/v2/users', headers=headers, data=create_user)
+    b = requests.post("https://gorest.co.in/public/v2/users", headers=headers, data=create_user)
     print(b.status_code)
     print(b.json())
 
+
 def test_function9():
-    for i in range(0,11):
+    for i in range(0, 11):
         print(i)
+
 
 def test_function10(create_user):
     print(create_user)
-
