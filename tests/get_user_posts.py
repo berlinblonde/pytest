eb617ca64a3d917f1997bb409a6730e666a8972f
olga <olgamursina@yandex.ru>
2023-08-22 21:11:53 +0200

    15/03/23

--- tests/get_user_posts.py
+++ tests/get_user_posts.py
@@ -1,9 +1,27 @@
-from tests.config import Forum
+from tests.config import GoRestService
+from tests.static import Data
 
 
 def test_function_101():
-    forum = Forum() #создаем экземпляр класса и присваиваем его переменной
-    posts = forum.get_user_posts(user_id=2659).json()# вызываем метод обьекта с аругментом (юзер_ид) и преобразуем в фомат json, результат сохраняя в переменную  posts
-    for post in posts: #проходимя по каждому элементу в списке
-        print(post)
+    posts = GoRestService().get_user_posts(params={})# вызываем метод обьекта с аругментом (юзер_ид) и преобразуем в фомат json, результат сохраняя в переменную  posts
+    assert posts.status_code == 200
+    assert len(posts.json())>0
+    for post in posts.json():
+        #assert post['user_id']==int(Data.user_id)
+        #assert len(post['body'])!=0
+        assert len(post['title'])!=0
 
+def test_function_190():
+    params={'page':1,'per_page':3}
+    posts = GoRestService().get_user_posts(params=params)# вызываем метод обьекта с аругментом (юзер_ид) и преобразуем в фомат json, результат сохраняя в переменную  posts
+    assert posts.status_code == 200
+    assert len(posts.json())>0
+    assert len(posts.json())<=params['per_page']
+
+
+def test_function_199():
+    params={'page':1,'per_page':0}
+    posts = GoRestService().get_user_posts(params=params)# вызываем метод обьекта с аругментом (юзер_ид) и преобразуем в фомат json, результат сохраняя в переменную  posts
+    assert posts.status_code == 200
+    assert len(posts.json())>0
+    assert len(posts.json())<=params['per_page']
