90d89ecbd5cf314f926f60df30e6daef3e843aa4
olga <olgamursina@yandex.ru>
2023-08-22 21:10:50 +0200

    15/03/23

--- tests/config.py
+++ tests/config.py
@@ -1,10 +1,11 @@
 import requests
-
+from tests.static import Data
 
 class GoRestService:
     url='https://gorest.co.in/public/v2'
     create_user='/users' #переменные-путь к ресурсу который будет добавлен к адресу
     get_users='/users/'
+    posts_path = '/posts/' #переменная-путь к ресурсу который будет добавлен к адресу
 
     def __init__(self): #метод конструктор
         pass
@@ -13,17 +14,8 @@ class GoRestService:
         r=requests.post(self.url+self.create_user, headers=headers, data=data) #функция внутри метода отправляет post добавляя заголовок и данные
         return r #возвращаем результат из метода
 
-
-
-class Forum:
-    url = "https://gorest.co.in/public/v2/users/"
-    posts_path = '/posts/' #переменная-путь к ресурсу который будет добавлен к адресу
-
-    def __init__(self): #метод-конструктор класса (через self далее можно обращаться внутри класса к функциональности текущего объекта)
-        pass
-
-    def get_user_posts(self, user_id): #метод класса, получение постов юзера с форума, принмает один аргумент
-        r = requests.get(self.url+str(user_id)+self.posts_path) #переменная внутри метода, где хранится результат запроса (переменная урл+аргрумент+переменная post_path) формируя полный урл
+    def get_user_posts(self, params): #метод класса, получение постов юзера с форума, принмает один аргумент
+        r = requests.get(self.url+self.get_users+Data.user_id+self.posts_path, params=params) #переменная внутри метода, где хранится результат запроса (переменная урл+аргрумент+переменная post_path) формируя полный урл
         return r #результат запроса возвращается из метода
 
 
